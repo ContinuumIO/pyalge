3e4974793710af311abc11a5e3a1131e33d05eaf
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-04-03 11:51:48 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(name='pyalge',
+      description="Pattern matching and Algebraic data types",
+      version="0.1",
+      classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Utilities",
+      ],
+      author="Continuum Analytics, Inc.",
+      py_modules=['alge'],
+      license="BSD",)
